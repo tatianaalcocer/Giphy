298c223babb6a0e6d5410b4b5dd3262510f1380f
Tatiana Marsh <tatiana.alcocer@gmail.com>
2016-09-13 18:03:09 -0400

    Layout finalized

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,15 +2,7 @@ $(document).ready(function(){
 
 //VARIABLES: ==========================================================================
 
-var animals = ['Otter', 'Hedgehog', 'Cow', 'Sloth', 'Cat', 'Dog', 'Panda'];
-
-
-
-
-
-
-
-// EVENT LISTENERS: ================================================================
+var animals = ['Otter', 'Hedgehog', 'Cow', 'Sloth', 'Cat', 'Dog', 'Panda', 'Penguin'];
 
 
 
@@ -39,7 +31,7 @@ function callApi(event) {
 	console.log(event)
 
 	var animal = $(event.currentTarget).data('name');
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=20";
 	
 	$.ajax({
         url: queryURL,
@@ -48,30 +40,72 @@ function callApi(event) {
     .done(function(response) {
 
         console.log(queryURL)
-        console.log(response)
 
-        var results = response.data;
-        console.log("results: " + results);
-
-
-
-        for (var i = 0; i < results.length; i++) {
-
-	        var animalDiv = $("<div>");
-
-	        var p = $("<p>" + results[i].rating + "</p>");
-
-	        var animalImage = $("<img>");
-	        animalImage.attr('src', results[i].images.fixed_height.url);
-
-	        animalDiv.append(p);
-	        animalDiv.append(animalImage);
-	        $(".gif-main").prepend(animalDiv);
-	        //--------------------------------
-	    }
+        renderAnimals(response.data);
 
     });
 }
+
+function renderAnimals(results) {
+
+	$('.gif-main').empty();
+
+    for (var i = 0; i < results.length; i++) {
+
+	    var animalDiv = $("<div>");
+	    animalDiv.addClass('animal-tile')
+
+	    var p = $("<p>Rating: " + results[i].rating + "</p>");
+
+	    var animalImage = $("<img>");
+	    animalImage.attr('src', results[i].images.fixed_width_still.url);
+	    animalDiv.append(p);
+	    animalDiv.append(animalImage);
+
+	    $(animalDiv).data('state', 'still');
+	    $(animalDiv).data('stillUrl', results[i].images.fixed_width_still.url);
+	    $(animalDiv).data('animatedUrl', results[i].images.fixed_width.url);
+
+	    $(".gif-main").prepend(animalDiv);
+
+    	$(animalDiv).on("click", toggleStillImage);
+
+	}
+
+
+}
+
+function toggleStillImage(event) {
+
+	// Assign the target div to a variable
+	var animal = event.currentTarget;
+
+	// Assign the image to a variable
+	var image = $(animal).find('img');
+
+	// Assign the stillUrl value to a variable
+	var stillUrl = $(animal).data('stillUrl');
+
+	// Assign the animatedUrl value to a variable
+	var animatedUrl = $(animal).data('animatedUrl');
+
+	if($(animal).data('state') === 'still') {
+
+		$(animal).data('state', 'animated');
+
+		// Change the image source to the still image
+		$(image).attr('src', animatedUrl);
+
+	} else {
+
+		$(animal).data('state', 'still');
+
+		// Change the image source the animated image
+		$(image).attr('src', stillUrl);
+	}
+}
+
+
 
 
 // MAIN SECTION: =============================================
